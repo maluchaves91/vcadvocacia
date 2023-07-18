b3c7036f271f872acdd6467dd4e6eb03762de4e8
Danielly Paes <108535530+DaniellyPaes@users.noreply.github.com>
2023-07-18 13:05:06 -0300

    Adicionado script, na sessão adm

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-    function scrollToSection(area) {
-        const section = document.getElementById(area);
-        if (section) {
-            section.scrollIntoView({ behavior: 'smooth' });
-        }
+function scrollToSection(area) {
+    const section = document.getElementById(area);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
     }
+}
 
+
+function redirecionarParaIndex() {
+    window.location.href = "index.html#area";
+}
